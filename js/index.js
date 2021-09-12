0e09955513227f5e64c068e8fa826772f943aa68
Hanan <63251521+HananPilakkadan@users.noreply.github.com>
2021-09-12 15:14:56 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,47 +1,52 @@
-$( document ).ready(function() {
-   var wow = new WOW({
-      boxClass: "wow", // animated element css class (default is wow)
-      animateClass: "animated", // animation css class (default is animated)
-      offset: 0, // distance to the element when triggering the animation (default is 0)
-      mobile: true, // trigger animations on mobile devices (default is true)
-      live: true, // act on asynchronously loaded content (default is true)
-      callback: function (box) {},
-      scrollContainer: null, // optional scroll container selector, otherwise use window,
-      resetAnimation: true, // reset animation on end (default is true)
-    });
-    wow.init();
+$(document).ready(function () {
+  var wow = new WOW({
+    boxClass: "wow", // animated element css class (default is wow)
+    animateClass: "animated", // animation css class (default is animated)
+    offset: 0, // distance to the element when triggering the animation (default is 0)
+    mobile: true, // trigger animations on mobile devices (default is true)
+    live: true, // act on asynchronously loaded content (default is true)
+    callback: function (box) {},
+    scrollContainer: null, // optional scroll container selector, otherwise use window,
+    resetAnimation: true, // reset animation on end (default is true)
+  });
+  wow.init();
 
-    $(".hamburger").on("click",function(){
-        $("body").toggleClass("menu-active");
-     });
-     $(".main,.gallery").click(function(){
-        $("body").removeClass("menu-active");
-     });
-     
-     $('.owl-carousel').owlCarousel({
-      margin:10,
-      items:4,
-      autoplay:true,
-      autoplayTimeout:2000,
-      loop:true,
-      fluidSpeed:true,
-      responsive:{
-         0:{
-             items:1
-         },
-         480:{
-            items:3
-         },
-         360:{
-            items:2
-         },
-         600:{
-             items:2
-         },
-         1000:{
-             items:5
-         }
-     }
-     });
+  $(".hamburger").on("click", function () {
+    $("body").toggleClass("menu-active");
+    $(".mobile-menu").css({ display: "block" });
+  });
+  $(".main,.gallery").click(function () {
+    $("body").removeClass("menu-active");
+  });
 
+  $(".owl-carousel").owlCarousel({
+    margin: 10,
+    items: 4,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    loop: true,
+    fluidSpeed: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      480: {
+        items: 3,
+      },
+      360: {
+        items: 2,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 4,
+      },
+    },
+  });
+
+
+  
+
+ 
 });
