a4f3ac2319ac05e8ee38652c3227d25788ca61f3
hananpilakkadan <hananpilakkadan@gmail.com>
2022-02-14 23:02:41 +0530

    updated-loader

--- js/index.js
+++ js/index.js
@@ -59,3 +59,9 @@ $(document).ready(function () {
     },
   });
 });
+
+
+var loader = document.getElementById("loader");
+	window.addEventListener("load", function () {
+		loader.style.display = "none";
+	})
